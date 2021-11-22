2d1eb85bfe0e2b9295bf48580b01a5c81a655248
Daisp <Daisp@qq,com>
2021-11-22 20:47:51 +0800

    first1

--- src/main/java/com/atguigu/App.java
+++ src/main/java/com/atguigu/App.java
@@ -8,8 +8,11 @@ public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello Git!" );
         System.out.println( "Hello Git!2" );
-        for(int i = 0;i < 10;i++){
-            System.out.prinl(i);
+        for(int i = 1;i <= 9;i++){
+            for (int j = 1;j <= i;j++){
+                System.out.print(j + "x"  + i + "=" + (i * j) + "\t");
+            }
+            System.out.println();
         }
     }
 }
