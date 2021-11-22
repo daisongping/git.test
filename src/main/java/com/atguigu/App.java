86c76e6737c35b48259bb8960ae4d01a107da6fb
Daisp <Daisp@qq,com>
2021-11-22 19:07:56 +0800

    first

--- src/main/java/com/atguigu/App.java
+++ src/main/java/com/atguigu/App.java
@@ -8,9 +8,5 @@ public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello Git!" );
         System.out.println( "Hello Git!2" );
-        System.out.println( "Hello Git!3" );
-        System.out.println( "Hello Git!4" );
-        System.out.println( "master" );
-     
     }
 }
