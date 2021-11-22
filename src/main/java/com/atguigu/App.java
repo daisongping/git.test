e7fae4f0477a277da4dac35982361ed730b0f499
dai_songping <94826442+daisongping@users.noreply.github.com>
2021-11-22 19:09:58 +0800

    Update App.java

--- src/main/java/com/atguigu/App.java
+++ src/main/java/com/atguigu/App.java
@@ -8,5 +8,8 @@ public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello Git!" );
         System.out.println( "Hello Git!2" );
+        for(int i = 0;i < 10;i++){
+            System.out.prinl(i);
+        }
     }
 }
